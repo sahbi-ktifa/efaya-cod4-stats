5ffc2b4012ca55d8cfa24f583e0d3e039c5443cb
Sahbi KTIFA <sahbi.ktifa@gmail.com>
2021-05-25 12:47:47 +0200

    Update game + winner

--- src/views/Championship.vue
+++ src/views/Championship.vue
@@ -315,6 +315,9 @@ export default class Championship extends Vue {
       if (this.seasonKey === "2020") {
         return "Les Sales gosses";
       }
+      if (this.seasonKey === "2021") {
+        return "Les pisseurs";
+      }
       return "";
     }
 
