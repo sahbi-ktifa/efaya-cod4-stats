a62992648efd351c1e22d96963828b818f1eede2
Sahbi Ktifa <sahbi@malt.com>
2024-09-04 11:51:12 +0200

    Fix stats parser + update game

--- src/services/InitGameParser.ts
+++ src/services/InitGameParser.ts
@@ -10,7 +10,8 @@ export default class InitGameParser implements LineParser {
 
     public parse(line: string, parsedData: ParsedData, ctx: ParserContext): void {
         // tslint:disable-next-line:max-line-length
-        // 575:01 InitGame: \_Admin\Gamoul\_Location\France\_Website\www.iclan.net\fs_game\mods/efaya_mod_test\g_compassShowEnemies\0\g_gametype\sr\gamename\Call of Duty 4\mapname\mp_naout\protocol\6\shortversion\1.7\sv_allowAnonymous\0\sv_disableClientConsole\0\sv_floodprotect\1\sv_hostname\^7i^1C^7lan.net ^1Efaya_mod\sv_maxclients\32\sv_maxPing\0\sv_maxRate\25000\sv_minPing\0\sv_privateClients\0\sv_punkbuster\0\sv_pure\0\sv_voice\1\ui_maxclients\32
+        // 575:01 InitGame: \_Admin\Gamoul\_Location\France\_Website\www.iclan.net\fs_game\mods/efaya_mod_test\g_compassShowEnemies\0\g_gametype\sr\gamename\Call of Duty 4\mapname\mp_naout\protocol\6\shortversion\1.7\sv_allowAnonymous\0\sv_disableClient
+        // \0\sv_floodprotect\1\sv_hostname\^7i^1C^7lan.net ^1Efaya_mod\sv_maxclients\32\sv_maxPing\0\sv_maxRate\25000\sv_minPing\0\sv_privateClients\0\sv_punkbuster\0\sv_pure\0\sv_voice\1\ui_maxclients\32
         const tokens = line.split("\\");
         // tslint:disable-next-line:one-variable-per-declaration
         let mod, map, gameType = "";
@@ -23,7 +24,7 @@ export default class InitGameParser implements LineParser {
                 map = tokens[i + 1];
             }
         }
-        console.log(parsedData.currentGame);
+        //console.log(parsedData.currentGame);
         if (!parsedData.currentGame) {
             // @ts-ignore
             parsedData.currentGame = new GameRef(mod, map, gameType);
--- src/services/KillParser.ts
+++ src/services/KillParser.ts
@@ -21,97 +21,102 @@ export default class KillParser implements LineParser {
         const tokens = splittedLine[splittedLine.length - 1].split(";");
         if (tokens) {
             for (const player of parsedData.currentGame.currentRound.players) {
+                // KILLER
                 if (player.playerRef.guid === tokens[5]) {
-                    player.kills++;
-                    if (tokens[3].length > 0 && tokens[3] === tokens[7]) {
-                        player.teamKills++;
-                        const teamKilled = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
-                        if (teamKilled) {
-                            teamKilled.teamKilled++;
-                        }
-                    } else {
-                        player.score += POINTS.KILL;
-                        const killed = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
-                        if (!parsedData.currentGame.currentRound.firstKiller) {
-                            parsedData.currentGame.currentRound.firstKiller =  player.playerRef;
+                    if (tokens[1] !== tokens[5]) {
+                        if (tokens[3].length > 0 && tokens[3] === tokens[7]) {
+                            player.teamKills++;
+                            const teamKilled = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
+                            if (teamKilled) {
+                                teamKilled.teamKilled++;
+                            }
+                        } else {
+                            player.kills++;
+                            player.score += POINTS.KILL;
+                            const killed = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
+                            if (!parsedData.currentGame.currentRound.firstKiller) {
+                                parsedData.currentGame.currentRound.firstKiller = player.playerRef;
+                                if (killed) {
+                                    parsedData.currentGame.currentRound.firstKilled = killed.playerRef;
+                                }
+                            }
+                            parsedData.currentGame.currentRound.lastKiller = player.playerRef;
                             if (killed) {
-                                parsedData.currentGame.currentRound.firstKilled =  killed.playerRef;
+                                parsedData.currentGame.currentRound.lastKilled = killed.playerRef;
                             }
                         }
-                        parsedData.currentGame.currentRound.lastKiller =  player.playerRef;
-                        if (killed) {
-                            parsedData.currentGame.currentRound.lastKilled =  killed.playerRef;
+                        if (tokens[12] === "head") {
+                            player.headShots++;
+                            player.score += POINTS.KILL_CONFIRMED;
+                            const headShoted = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
+                            if (headShoted) {
+                                headShoted.headShoted++;
+                            }
                         }
-                    }
-                    if (tokens[12] === "head") {
-                        player.headShots++;
-                        const headShoted = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
-                        if (headShoted) {
-                            headShoted.headShoted++;
+                        if (tokens[9] === "frag_grenade_mp" || tokens[9] === "frag_grenade_short_mp") {
+                            player.grenadeKills++;
+                            const grenaded = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
+                            if (grenaded) {
+                                grenaded.grenaded++;
+                            }
                         }
-                    }
-                    if (tokens[9] === "frag_grenade_mp" || tokens[9] === "frag_grenade_short_mp") {
-                        player.grenadeKills++;
-                        const grenaded = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
-                        if (grenaded) {
-                            grenaded.grenaded++;
+                        if (tokens[9].indexOf("usp") === 0 || tokens[9].indexOf("beretta") === 0
+                            || tokens[9].indexOf("colt") === 0 || tokens[9].indexOf("deserteagle") === 0) {
+                            player.pistolKills++;
                         }
-                    }
-                    if (tokens[9].indexOf("usp") === 0 || tokens[9].indexOf("beretta") === 0
-                        || tokens[9].indexOf("colt") === 0 || tokens[9].indexOf("deserteagle") === 0) {
-                        player.pistolKills++;
-                    }
-                    if (tokens[9].indexOf("m16_") === 0 || tokens[9].indexOf("m4_") === 0
-                        || tokens[9].indexOf("g3_") === 0 || tokens[9].indexOf("m14_") === 0
-                        || tokens[9].indexOf("ak47") === 0 || tokens[9].indexOf("g36c") === 0
-                        || tokens[9].indexOf("mp44") === 0) {
-                        player.rifleKills++;
-                    }
-                    if (tokens[9].indexOf("mp5") === 0 || tokens[9].indexOf("uzi") === 0
-                        || tokens[9].indexOf("p90") === 0 || tokens[9].indexOf("ak74u") === 0
-                        || tokens[9].indexOf("skorpion") === 0) {
-                        player.smgKills++;
-                    }
-                    if (tokens[9].indexOf("winchester") === 0 || tokens[9].indexOf("m1014") === 0) {
-                        player.shotgunKills++;
-                    }
-                    if (tokens[9].indexOf("barrett") === 0 || tokens[9].indexOf("dragunov") === 0
-                        || tokens[9].indexOf("m21") === 0 || tokens[9].indexOf("m40a3") === 0
-                        || tokens[9].indexOf("remington") === 0) {
-                        player.sniperKills++;
-                    }
-                    if (tokens[11] === "MOD_MELEE") {
-                        player.meleeKills++;
-                        const knifed = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
-                        if (knifed) {
-                            knifed.knifed++;
+                        if (tokens[9].indexOf("m16_") === 0 || tokens[9].indexOf("m4_") === 0
+                            || tokens[9].indexOf("g3_") === 0 || tokens[9].indexOf("m14_") === 0
+                            || tokens[9].indexOf("ak47") === 0 || tokens[9].indexOf("g36c") === 0
+                            || tokens[9].indexOf("mp44") === 0) {
+                            player.rifleKills++;
                         }
-                    }
-                    if (tokens[11] === "MOD_EXPLOSIVE") {
-                        player.collateralKills++;
-                    }
-                    if (tokens[11] === "MOD_TRIGGER_HURT" || tokens[11] === "MOD_FALLING"
-                        || tokens[11] === "MOD_SUICIDE") {
+                        if (tokens[9].indexOf("mp5") === 0 || tokens[9].indexOf("uzi") === 0
+                            || tokens[9].indexOf("p90") === 0 || tokens[9].indexOf("ak74u") === 0
+                            || tokens[9].indexOf("skorpion") === 0) {
+                            player.smgKills++;
+                        }
+                        if (tokens[9].indexOf("winchester") === 0 || tokens[9].indexOf("m1014") === 0) {
+                            player.shotgunKills++;
+                        }
+                        if (tokens[9].indexOf("barrett") === 0 || tokens[9].indexOf("dragunov") === 0
+                            || tokens[9].indexOf("m21") === 0 || tokens[9].indexOf("m40a3") === 0
+                            || tokens[9].indexOf("remington") === 0) {
+                            player.sniperKills++;
+                        }
+                        if (tokens[11] === "MOD_MELEE") {
+                            player.meleeKills++;
+                            player.score += POINTS.KILL_CONFIRMED;
+                            const knifed = parsedData.currentGame.currentRound.players.filter((p) => p.playerRef.guid === tokens[1])[0];
+                            if (knifed) {
+                                knifed.knifed++;
+                            }
+                        }
+                        if (tokens[11] === "MOD_EXPLOSIVE") {
+                            player.collateralKills++;
+                        }
+                        if (!player.weaps[tokens[9]]) {
+                            player.weaps[tokens[9]] = 0;
+                        }
+                        player.weaps[tokens[9]]++;
+                        if (tokens[12] !== "none") {
+                            if (!player.parts[tokens[12]]) {
+                                player.parts[tokens[12]] = 0;
+                            }
+                            player.parts[tokens[12]]++;
+                        }
+                        if (!player.prey[tokens[1]]) {
+                            player.prey[tokens[1]] = 0;
+                        }
+                        player.prey[tokens[1]]++;
+                        if (!player.quickestKill) {
+                            player.quickestKill = TimeUtils.getDiffTime(parsedData.currentGame.currentRound.startTime, line);
+                        }
+                    } else {
                         player.suicides++;
-                    }
-                    if (!player.weaps[tokens[9]]) {
-                        player.weaps[tokens[9]] = 0;
-                    }
-                    player.weaps[tokens[9]]++;
-                    if (tokens[12] !== "none") {
-                        if (!player.parts[tokens[12]]) {
-                            player.parts[tokens[12]] = 0;
-                        }
-                        player.parts[tokens[12]]++;
-                    }
-                    if (!player.prey[tokens[1]]) {
-                        player.prey[tokens[1]] = 0;
-                    }
-                    player.prey[tokens[1]]++;
-                    if (!player.quickestKill) {
-                        player.quickestKill = TimeUtils.getDiffTime(parsedData.currentGame.currentRound.startTime, line);
+                        player.deaths++;
                     }
                 }
+                // Killed
                 if (player.playerRef.guid === tokens[1]) {
                     player.deaths++;
                     if (!player.nemesis[tokens[5]]) {
